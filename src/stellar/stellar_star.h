96b237958beb998a8d0a757fbfc1979e412814a6
tdiego1 <tdiego001@gmail.com>
2026-01-02 09:05:22 -0700

    Update copyright and licensing information across the project
    - Standardize copyright notices to remove "All rights reserved."
    - Add GNU General Public License details to all relevant files
    - Ensure consistent formatting of licensing information throughout the codebase

--- src/stellar/stellar_star.h
+++ src/stellar/stellar_star.h
@@ -9,13 +9,23 @@
  * based on mass and age, following standard astrophysical models.
  *
  * @author Diego Torres
- * @date 2026
- * @copyright Copyright (C) 2026 Diego Torres. All rights reserved.
+ * @date 2025
+ * @copyright Copyright (C) 2025 Diego Torres
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * For commercial licensing options, please contact: tdiego001@gmail.com
  */
 /**************************************************************************************************/
 
